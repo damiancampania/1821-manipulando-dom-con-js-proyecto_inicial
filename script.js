8e5134eae391705c8fda498965a3bf420fa77b0f
damiancampania <damiancampania@gmail.com>
2023-03-30 21:08:57 -0300

    clase 04 completada

--- script.js
+++ script.js
@@ -1,33 +1,75 @@
 const btn = document.querySelector("[data-form-btn]");
 const input = document.querySelector("[data-form-input]");
-console.log(btn);
-
-btn
-
 
 const createTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector("[data-form-input]");
     const value = input.value;
-    const list =document.querySelector ("[data-list]")
+    
+    const list =document.querySelector ("[data-list]");
+    
     const task = document.createElement ("li");
     task.classList.add("card");
     input.value = "";
-    const content = ` <div>
+    const taskContent = document.createElement ("div");
+    const titleTask = document.createElement ("span")
+    list.appendChild(task);
+    task.appendChild (taskContent) ;
+    taskContent.appendChild(checkComplete() );
+    taskContent.appendChild (titleTask);
+    
+    titleTask.classList.add("task");
+    titleTask.innerText= value;
+  
+
+   /* const content =  /* ` <div> /////////////////////////////estructura del content que luego se reemplaza en constantes
     <i class="far fa-check-square icon"></i>
     <span class="task" >${value}</span>
   </div>
   <i class="fas fa-trash-alt trashIcon icon" ></i>`
-task.innerHTML = content;
-list.appendChild(task);
-    console.log (value)
+   `  
+  <i class="fas fa-trash-alt trashIcon icon" ></i>`*/
+
+task.appendChild(basura());
+//task.innerHTML = content;
+
+
+  
+
+
 
     }
 
-    console.log(btn);
+    
 // arrow o funciones anonimas =>
 btn.addEventListener("click", createTask); 
+const checkComplete = () => {
+  const i = document.createElement('i');
+  i.classList.add('far', 'fa-check-square', 'icon' );
+  i.addEventListener("click", completeTask);
+  
+  return i;
 
+}
+
+const completeTask = (event) => {
+const element = event.target;
+element.classList.toggle("fas");
+element.classList.toggle ("completeIcon");
+element.classList.toggle ("far");
+
+}
+
+const basura = () => {
+  const i = document.createElement('i');
+  i.classList.add('fas' );
+  i.classList.add('fa-trash-alt');
+  i.classList.add('trashIcon');
+  i.classList.add('icon');
+  
+  return i;
+
+}
 
 
 
